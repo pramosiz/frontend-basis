11106fff0c56c8ecd05d90abddbe4df3a20831fe
pramosiz <ramosizquierdop@gmail.com>
2025-01-12 14:05:29 +0100

    implemented jQuery

--- JavaScript/Calculator/resources/javascripts/calculator.js
+++ JavaScript/Calculator/resources/javascripts/calculator.js
@@ -1,64 +1,64 @@
 function showResult(result) {
-    document.getElementById("result").value = result
+    $("#result").val(result)
 }
 
 function add() {
-    let num1 = +document.getElementById("field1").value
-    let num2 = +document.getElementById("field2").value
+    let num1 = +$("#field1").val()
+    let num2 = +$("#field2").val()
     showResult(num1 + num2)
 }
 
 function subtract() {
-    let num1 = +document.getElementById("field1").value
-    let num2 = +document.getElementById("field2").value
+    let num1 = +$("#field1").val()
+    let num2 = +$("#field2").val()
     showResult(num1 - num2)
 }
 
 function multiply() {
-    let num1 = +document.getElementById("field1").value
-    let num2 = +document.getElementById("field2").value
+    let num1 = +$("#field1").val()
+    let num2 = +$("#field2").val()
     showResult(num1 * num2)
 }
 
 function divide() {
-    let num1 = +document.getElementById("field1").value
-    let num2 = +document.getElementById("field2").value
+    let num1 = +$("#field1").val()
+    let num2 = +$("#field2").val()
     showResult(num1 / num2)
 }
 
 function root() {
-    let num1 = +document.getElementById("field1").value
+    let num1 = +$("#field1").val()
     showResult(Math.sqrt(num1))
 }
 
-function pow() {
-    let num1 = +document.getElementById("field1").value
-    let num2 = +document.getElementById("field2").value
+function power() {
+    let num1 = +$("#field1").val()
+    let num2 = +$("#field2").val()
     showResult(Math.pow(num1, num2))
 }
 
 function absoulte() {
-    let num1 = +document.getElementById("field1").value
+    let num1 = +$("#field1").val()
     showResult(Math.abs(num1))
 }
 
 function random() {
-    let num1 = +document.getElementById("field1").value
-    let num2 = +document.getElementById("field2").value
+    let num1 = +$("#field1").val()
+    let num2 = +$("#field2").val()
     showResult(Math.floor(Math.random() * (num2 - num1 + 1)) + num1)
 }
 
 function round() {
-    let num1 = +document.getElementById("field1").value
+    let num1 = +$("#field1").val()
     showResult(Math.round(num1))
 }
 
 function ceil() {
-    let num1 = +document.getElementById("field1").value
+    let num1 = +$("#field1").val()
     showResult(Math.ceil(num1))
 }
 
 function floor() {
-    let num1 = +document.getElementById("field1").value
+    let num1 = +$("#field1").val()
     showResult(Math.floor(num1))
 }
